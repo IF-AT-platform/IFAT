362b670dc2d939c96bad7bbc5b47700ef12df776
Cecilia Yin <41213946+cecilia-yin@users.noreply.github.com>
2021-07-21 16:02:18 +1200

    Update index.js

--- index.js
+++ index.js
@@ -18,6 +18,15 @@ connection.once('open', () => {
     console.log("MongoDB database connection established successfully");
 })
 
+// Accessing the path module
+const path = require("path");
+
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+// Step 2:
+app.get("*", function (request, response) {
+    response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
